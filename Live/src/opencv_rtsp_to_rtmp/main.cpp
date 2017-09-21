24106f8bfed8c05e9b0161258da3a83cc0f1be9c
Drew.Chiang <jiangchuna@126.com>
2017-09-21 12:13:07 +0800

    push stream.

--- Live/src/opencv_rtsp_to_rtmp/main.cpp
+++ Live/src/opencv_rtsp_to_rtmp/main.cpp
@@ -7,6 +7,7 @@ extern "C"
 {
 #include <libswscale/swscale.h>
 #include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
 }
 //预处理失败，找不到头文件；编译失败语法错误；链接失败，找不到lib文件；执行失败，找不到动态链接库。
 using namespace cv;
@@ -16,8 +17,9 @@ using namespace std;
 #pragma comment(lib, "swscale.lib")
 #pragma comment(lib, "avcodec.lib")
 #pragma comment(lib, "avutil.lib")
+#pragma comment(lib, "avformat.lib")
 
-int main(int argc, char *argv)
+int main(int argc, char **argv)
 {
     const char *inUrl = "rtsp://admin:ASZSCS@192.168.3.3";
     const char *outUrl = "rtmp://106.14.33.215:1935/live/fmlive";
@@ -31,8 +33,20 @@ int main(int argc, char *argv)
     //输出的数据结构
     AVFrame *yuv = NULL;
 
+    //注册所有的编解码器;
+    avcodec_register_all();
+
+    //注册所有的封装器
+    av_register_all();
+
+    //注册所有网络协议
+    avformat_network_init();
+
     //编码器上下文
     AVCodecContext *codec_ctx = NULL;
+
+    //rtmp flv封装器
+    AVFormatContext *ofmt_ctx = NULL;
 
 	VideoCapture cam;
 
@@ -80,21 +94,21 @@ int main(int argc, char *argv)
         }
 
         ///4.初始化编码上下文
-        //        avcodec_register_all();
-        //1.找到编码器
+
+        //a.找到编码器
         AVCodec *codec = avcodec_find_encoder(AV_CODEC_ID_H264);
 
         if(!codec){
             throw exception("Can't find h264 encoder!");
         }
 
-        //2.创建编码器上下文
+        //b.创建编码器上下文
         codec_ctx = avcodec_alloc_context3(codec);
         if(!codec_ctx){
             throw exception("avcodec_alloc_context3 failed!");
         }
 
-        //3.配置编码器参数
+        //c.配置编码器参数
         codec_ctx->flags |= AV_CODEC_FLAG_GLOBAL_HEADER;//全局参数
         codec_ctx->codec_id = codec->id;
         codec_ctx->thread_count = 8;//编码线程数量
@@ -104,13 +118,62 @@ int main(int argc, char *argv)
         codec_ctx->time_base = {1,fps};//pts以什么数进行计算
         codec_ctx->framerate = {fps,1};//帧率
         codec_ctx->gop_size = 50;//画面组的大小，多少帧一个关键帧
-
-        //4.打开编码器上下文
-
-
+        codec_ctx->max_b_frames = 0; //B帧
+        codec_ctx->pix_fmt = AV_PIX_FMT_YUV420P;
 
 
+        //d.打开编码器上下文
+         ret = avcodec_open2(codec_ctx, NULL, NULL);
+         if(ret != 0){
+            char buf[1024] = {0};
+            av_strerror(ret, buf, sizeof(buf) - 1);
+            throw exception(buf);
+         }
+        cout << "avcodec_open2 success" << endl;
 
+        ///5.封装器和视频流配置
+        //a.创建输出封装器上下文
+        ret = avformat_alloc_output_context2(&ofmt_ctx, 0, "flv", outUrl);
+        if(ret != 0){
+            char buf[1024] = {0};
+            av_strerror(ret, buf, sizeof(buf) - 1);
+            throw exception(buf);
+         }
+        //b.添加视频流
+        AVStream *vstream = avformat_new_stream(ofmt_ctx, NULL);
+        if(!vstream){
+            throw exception("avformat_new_stream failed");
+        }
+
+        vstream->codecpar->codec_tag = 0;
+        //从编码器复制参数
+        avcodec_parameters_from_context(vstream->codecpar, avcodec);
+        av_dump_format(ofmt_ctx, 0, outUrl, 1);
+
+        //打开rtmp的网络输出IO
+        ret = avio_open(&ofmt_ctx->pb, outUrl, AVIO_FLAG_WRITE);
+        if(ret != 0){
+            char buf[1024] = {0};
+            av_strerror(ret, buf, sizeof(buf) -1);
+            throw exception(buf);
+        }
+
+        //写入封装头
+        ret = avformat_write_header(ofmt_ctx, NULL);
+        if(ret != 0){
+            char buf[1024] = {0};
+            av_strerror(ret, buf, sizeof(buf) -1);
+            throw exception(buf);
+        }
+
+
+
+
+
+        AVPacket pkt;
+        memset(&pkt, 0, sizeof(pkt));
+
+        int vpts = 0;
         for(;;){
             //读取rtsp视频帧，解码视频帧
             if(!cam.grab()){//只做解码
@@ -145,8 +208,28 @@ int main(int argc, char *argv)
             }
             cout << h << " " << flush;
 
+            ///h264编码
+            yuv->pts = vpts++;
 
+            ret = avcodec_send_frame(codec_ctx, yuv);
+            if(ret != 0){
+                continue;
+            }
 
+            ret = avcodec_receive_packet(codec_ctx, &pkt);
+            if(ret != 0 || pkt.size > 0){
+                cout << "*" << pkt.size << flush;
+            }else{
+                continue;
+            }
+
+            ///推流
+            pkt->pts = av_rescale_q(pkt.pts, codec_ctx->time_base, vstream->time_base);
+            pkt->dts = av_rescale_q(pkt.dts, codec_ctx->time_base, vstream->time_base);
+            ret = av_interleaved_write_frame(ofmt_ctx, &pkt);
+            if(ret == 0){
+                cout<< "#" << endl;
+            }
         }
 
 
@@ -164,6 +247,10 @@ int main(int argc, char *argv)
             avcodec_free_context(&codec_ctx);
         }
 
+        if(vc){
+            avio_closep(&ofmt_ctx->pb);
+            avcodec_free_context(&vc);
+        }
 
         cerr << ex.what() << endl;
     }
